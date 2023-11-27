01e4c4f4e26f1ae5ab6434783327b3c4264fa689
Jakub Jirous <info@jakubjirous.dev>
2023-11-26 17:40:18 -0800

    feat(clerk): add sign in page powered by clerk

---
+++ app/(auth)/sign-in/[[...sign-in]]/page.tsx
@@ -0,0 +1,14 @@
+import { SignIn } from "@clerk/nextjs";
+import type { Metadata } from "next";
+
+export const metadata: Metadata = {
+  title: "Sign In",
+};
+
+export default function Page() {
+  return (
+    <>
+      <SignIn afterSignInUrl="/dashboard" />
+    </>
+  );
+}
--- app/(auth)/sign-in/page.tsx
+++
@@ -1,3 +0,0 @@
-export default function Page() {
-  return <>Sign In - Login</>;
-}
