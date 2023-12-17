60633328b668bfdd672de97d8c39be45ad05d26d
Jakub Jirous <info@jakubjirous.dev>
2023-12-17 12:47:05 -0800

    feat(invoice-list): add layout

--- app/(privateRoutes)/layout.tsx
+++ app/(privateRoutes)/layout.tsx
@@ -1,7 +1,16 @@
+import Navbar from "@/app/ui/navbar";
+
 export default function PrivateLayout({
   children,
 }: {
   children: React.ReactNode;
 }) {
-  return <main>{children}</main>;
+  return (
+    <main className="flex min-h-screen flex-col lg:flex-row">
+      <Navbar />
+      <div className="container mx-auto flex min-h-full flex-1 px-6 py-8 md:px-12 md:py-14 lg:px-20 lg:py-16">
+        {children}
+      </div>
+    </main>
+  );
 }
