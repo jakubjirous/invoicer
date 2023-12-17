12b618c6f86cd7843f19bdd7e077c555701f5192
Jakub Jirous <info@jakubjirous.dev>
2023-12-17 12:44:28 -0800

    feat(clerk): change appearance

--- app/ui/clerk-sign-in.tsx
+++ app/ui/clerk-sign-in.tsx
@@ -14,20 +14,20 @@ export default function ClerkSignIn() {
 
   return (
     <SignIn
-      afterSignInUrl="/dashboard"
+      afterSignInUrl="/invoices"
       appearance={{
         baseTheme: theme === "dark" ? dark : undefined,
         elements: {
           socialButtonsIconButton:
-            "focus-visible:ring-4 focus-visible:ring-cornflower/50 duration-500",
+            "focus-visible:ring-4 focus-visible:ring-cornflower/50 transition duration-500",
           formFieldLabel:
             "text-ship font-spartan text-[0.8125rem] font-medium leading-[0.9375rem] tracking-[-0.00625rem]",
           formFieldInput:
-            "px-[1.25rem] text-vulcan font-spartan font-bold leading-[0.9375rem] tracking-[-0.01563rem] rounded-[0.25rem] border-selago h-12 focus:border-heliotrope duration-500 dark:bg-mirage dark:text-white dark:border-ebony dark:focus:border-cornflower",
+            "px-[1.25rem] text-vulcan font-spartan font-bold leading-[0.9375rem] tracking-[-0.01563rem] rounded-[0.25rem] border-selago h-12 focus:border-heliotrope transition duration-500 dark:bg-mirage dark:text-white dark:border-ebony dark:focus:border-cornflower",
           formButtonPrimary:
-            "capitalize inline-flex flex-row items-center justify-center text-[0.938rem] font-spartan font-bold leading-[0.9375rem] tracking-[-0.01563rem] rounded-[1.5rem] h-12 transition-[background,color] duration-500 focus-visible:rounded-3xl focus-visible:outline-none focus-visible:ring-4 text-white px-6 bg-cornflower hover:bg-heliotrope focus-visible:ring-cornflower/50",
+            "capitalize inline-flex flex-row items-center justify-center text-[0.938rem] font-spartan font-bold leading-[0.9375rem] tracking-[-0.01563rem] rounded-[1.5rem] h-12 transition-[background,color] transition duration-500 focus-visible:rounded-3xl focus-visible:outline-none focus-visible:ring-4 text-white px-6 bg-cornflower hover:bg-heliotrope focus-visible:ring-cornflower/50",
           footerActionLink:
-            "active:text-cornflower hover:no-underline focus:ring-0 focus:outline-none focus-visible:rounded-[0.625rem] focus-visible:ring-4 focus-visible:ring-cornflower/50 duration-500",
+            "active:text-cornflower hover:no-underline focus:ring-0 focus:outline-none focus-visible:rounded-[0.625rem] focus-visible:ring-4 focus-visible:ring-cornflower/50 transition duration-500",
         },
       }}
     />
