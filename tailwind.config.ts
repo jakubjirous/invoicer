044406b4d487ef299ccfe83386e4071919b0fc4e
Jakub Jirous <info@jakubjirous.dev>
2023-12-17 12:45:06 -0800

    style: update tailwind config

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -1,5 +1,5 @@
+import { createFluidValue } from "@/app/utils/create-fluid-value";
 import type { Config } from "tailwindcss";
-import { createFluidValue } from "./app/utils/create-fluid-value";
 
 const config: Config = {
   darkMode: ["class"],
@@ -26,22 +26,36 @@ const config: Config = {
         whisper: "#F8F8FB",
         steel: "#141625",
 
+        fiord: "#494E6E", // navbar border
         oxford: "#373B53", // side panel bg
+
+        // status
+        shamrock: "#33D69F", // paid
+        pizazz: "#FF8F00", // pending
       },
       fontFamily: {
         spartan: ["var(--font-league-spartan)"],
       },
       fontSize: {
         "fluid-hero-heading": createFluidValue(40, 80),
-        "fluid-h1": createFluidValue(28, 36),
+        "fluid-h1": createFluidValue(24, 36),
         "fluid-h2": createFluidValue(18, 24),
         "fluid-h3": createFluidValue(15, 15),
         "fluid-h4": createFluidValue(15, 15),
-        "fluid-body1": createFluidValue(13, 18),
-        "fluid-body2": createFluidValue(13, 15),
+        "fluid-body1": createFluidValue(13, 13),
+        "fluid-body2": createFluidValue(13, 13),
       },
       borderRadius: {
         "3xl": "1.25rem",
+      },
+      gridTemplateColumns: {
+        "invoice-item": "1fr minmax(min-content, 6.5rem)",
+        "invoice-item-md":
+          "repeat(5, minmax(min-content, 1fr)), repeat(1, minmax(min-content, max-content))",
+      },
+      gridTemplateRows: {
+        "invoice-item":
+          "minmax(min-content, 1.5rem) minmax(min-content, 1.5rem) minmax(min-content, 1.5rem)",
       },
     },
   },
